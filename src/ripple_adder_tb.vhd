ea56346c282101006fc1ae12cf4d9d0cf367ce51
Fields <c27samuel.fields@afacademy.af.edu>
2025-02-13 23:03:18 -0700

    Attempt 3

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -51,7 +51,7 @@ begin
        -- TODO, a few other test cases
        w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"1" and w_Cout = '0') report "failure on easy-forget case" severity failure;
-	   w_addends <= x"0A"; w_Cin <= '1'; wait for 10 ns;
+	   w_addends <= x"0A"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"B" and w_Cout = '1') report "failure on random case" severity failure;
 	       
 	       
