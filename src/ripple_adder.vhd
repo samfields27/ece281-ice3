ca4eef8fc2e7d897bb42a413ceb0f61ff450f71e
Fields <c27samuel.fields@afacademy.af.edu>
2025-02-13 22:46:45 -0700

    Attempt 3

--- src/ripple_adder.vhd
+++ src/ripple_adder.vhd
@@ -61,7 +61,9 @@ begin
         Cin   => w_carry(2),
         S     => S(3),
         Cout  => w_carry(3)
-        Cout  <= w_carry(3)
     );
+    Cout  <= w_carry(3)
+    
+    
 
 end Behavioral;
